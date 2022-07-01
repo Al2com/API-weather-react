e3ad276200d9b49c906b91c28cf899a024272fee
Al2com <acomenge@iesfuentesanluis.org>
2022-07-01 03:07:54 +0200

    Empezando Proyecto

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React,{useEffect, useState} from "react";
+
+const getData = async () => {
+  try {
+    const response = await fetch('https://api.weatherbit.io/v2.0/forecast/daily?city=Guadasuar,ES&key=6e266f9ea6524f13857716617f889692');
+    const datos = await response.json();
+    return datos;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function App() {
+  const [datos, setData] = useState([]);
+  const fetchData = async () => {
+    try {
+      const data = await getData();
+      console.log(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+  fetchData ()
+
+}, []  )
+  
+
+
+  return (
+    <>
+    <header>
+      <h1>Tiempo en el mundo</h1>
+      </header>
+    </>
+  );}
+
+export default App;
