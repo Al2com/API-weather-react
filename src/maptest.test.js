b261a0e011d88314882963133b80a03a38680727
Al2com <acomenge@iesfuentesanluis.org>
2022-07-23 16:02:05 +0200

    test simple funcionando

--- src/maptest.test.js
+++ src/maptest.test.js
@@ -2,9 +2,11 @@
 //given -----> dado que
 //when  ----> cuando
 //then  ----->entonces
+git add .add
 const maptest = () => {
-  return 'Elige una ciudad:';
+  return "Elige una ciudad:";
 };
+
 
 describe('maptest', () => {
   it("si aparece Elige una ciudad", () => {
