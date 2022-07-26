a8be00ef504d5924c83cc85a3c11225137db0779
Al2com <acomenge@iesfuentesanluis.org>
2022-07-27 00:55:40 +0200

    begining-styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,8 +6,8 @@ const Nav = (props) => {
   <form onSubmit={props.handleOnSubmit}>
     <label htmlFor="search">Elige una ciudad:</label>
     <input type="search" name="search" id="search" />
-      <button type="submit">Buscar</button>
-    </form>
+    <button type="submit">Buscar</button>
+   </form>
 
   )
 };
